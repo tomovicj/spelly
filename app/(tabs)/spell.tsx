fd6f7f3aa8973eda1d9193468e54d95a4edad12f
Tomovic <jovantomovic5@gmail.com>
2024-11-30 22:59:29 +0100

    Refactor spelling component and add words listing page

--- app/(tabs)/spell.tsx
+++ app/(tabs)/spell.tsx
@@ -1,82 +1,49 @@
-import { useState, useEffect } from "react";
-import {
-  View,
-  Text,
-  Pressable,
-  TextInput,
-  ActivityIndicator,
-} from "react-native";
-import { router } from "expo-router";
-import axios from "axios";
-import styles from "@/theme/styles";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import React from "react";
+import { Link } from "expo-router";
 
-export default function Index() {
-  const [word, setWord] = useState<string | undefined>();
-  const [isLoading, setLoading] = useState<boolean>(false);
-  const [error, setError] = useState<string | null>(null);
-  const [wordLength, setWordLength] = useState<number>(5);
-
-  const fetchData = async () => {
-    setLoading(true);
-    setError(null);
-    try {
-      const response = await axios.get(
-        `https://random-word-api.herokuapp.com/word?lang=en&length=${wordLength}`
-      );
-      const word = response.data[0];
-      setWord(word);
-    } catch (err) {
-      if (axios.isAxiosError(err)) {
-        setError(err.message || "An Axios error occurred");
-      } else {
-        setError("An unexpected error occurred");
-      }
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    if (word !== undefined) router.push(`/spelling/spell/${word}`);
-  }, [word]);
-
-  if (isLoading) {
-    return (
-      <View style={styles.container}>
-        <ActivityIndicator size="large" color="#007bff" />
-        <Text style={styles.text}>Loading...</Text>
-      </View>
-    );
-  }
-
-  if (error) {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.text}>Error: {error}</Text>
-      </View>
-    );
-  }
-
+const spell = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.header}>Spelling</Text>
-      <Text style={styles.text}>
-        Enter the length of the word you want to spell:
-      </Text>
-      <TextInput
-        style={styles.input}
-        onChangeText={(length) => {
-          const parsedLength = parseInt(length, 10);
-          if (isNaN(parsedLength)) setWordLength(0);
-          else setWordLength(parsedLength);
-        }}
-        value={wordLength.toString()}
-        keyboardType="numeric"
-        maxLength={2}
-      />
-      <Pressable onPress={() => fetchData()} style={styles.button}>
-        <Text style={styles.text}>Start with spelling</Text>
-      </Pressable>
+      <Text style={styles.headerText}>Spelly</Text>
+      <Link href="/spelling/spell/test" asChild>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Start Spelling</Text>
+        </TouchableOpacity>
+      </Link>
+      <Link href="/spelling/words" asChild>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>List of Words</Text>
+        </TouchableOpacity>
+      </Link>
     </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  headerText: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 40,
+  },
+  button: {
+    backgroundColor: "lightblue",
+    padding: 20,
+    borderRadius: 5,
+    margin: 10,
+    width: 300,
+  },
+  buttonText: {
+    color: "#000",
+    fontSize: 16,
+    textAlign: "center",
+  },
+});
+
+export default spell;
---
+++ app/spelling/words.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const words = () => {
+  return (
+    <View>
+      <Text>words</Text>
+    </View>
+  )
+}
+
+export default words
