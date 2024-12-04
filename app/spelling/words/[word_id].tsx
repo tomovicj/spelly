49a93c424c1dd3d0c035fb8b8ab19dca77d0847a
Tomovic <jovantomovic5@gmail.com>
2024-12-04 14:54:39 +0100

    Add audio playback feature to word page

--- app/spelling/words/[word_id].tsx
+++ app/spelling/words/[word_id].tsx
@@ -1,7 +1,8 @@
 import ToggleFavorite from "@/utils/ToggleFavorite";
 import { useLocalSearchParams, Stack, router } from "expo-router";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { useAudioPlayer } from "expo-audio";
 
 type Props = {
   word_id: string;
@@ -11,8 +12,18 @@ type Props = {
 };
 
 function word() {
-  const { word_id, word, definition, is_favorite } = useLocalSearchParams<Props>();
+  const { word_id, word, definition, is_favorite } =
+    useLocalSearchParams<Props>();
   if (!word_id || !word) router.back();
+
+  const player = useAudioPlayer(
+    `https://spelly.jovantomovic.com/audio/tiktok/${word.toLowerCase()}.mp3`
+  );
+  const playSound = () => {
+    player.seekTo(0);
+    player.play();
+  };
+
   return (
     <>
       <Stack.Screen
@@ -30,7 +41,16 @@ function word() {
         }}
       />
       <View style={styles.container}>
-        <Text style={styles.word}>{word}</Text>
+        <View style={styles.heading}>
+          <Text style={styles.word}>{word}</Text>
+          <TouchableOpacity onPress={playSound}>
+            <MaterialCommunityIcons
+              name="volume-high"
+              size={32}
+              color="black"
+            />
+          </TouchableOpacity>
+        </View>
         <Text style={styles.definition}>{definition}</Text>
       </View>
     </>
@@ -40,8 +60,12 @@ function word() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    padding: 20,
+  },
+  heading: {
+    flexDirection: "row",
+    gap: 20,
+    marginBottom: 10,
   },
   word: {
     fontSize: 24,
